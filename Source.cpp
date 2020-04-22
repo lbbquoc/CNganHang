65d08f996665db99f0a155be0063e9e602dd5ab8
minh thanh <minhthanhd013@gmail.com>
2020-04-22 18:06:06 +0700

    Da sua file source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,41 @@
-
-
-
-#include "QuanLy.h"
-#include <time.h>
-#include "QuanLy.h"
-
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <string>
+#include "time.h"
+#include <stdlib.h> 
+//#include "Header.h"
+//#include "Header1.h"
+#include "Header2.h"
+#include "Header3.h"
+#include <vector>
 
 using namespace std;
+
+
 int main()
 {
-	CNganHang qly;
-	qly.Nhap();
-	qly.Xuat();
 	
+	/*STK_KhongKyHan pp1;
+	pp1.nhap();
+	cout << pp1.tienLai() << endl;
+
 	
-	return 0;
+	STK_CoKyHan pp2;
+	pp2.nhap();
+	cout << pp2.tienLai() << endl;*/
+	CNganHang a;
+	a.input();
+	
+	cout<<"Tong so tien lai ma ngan hang phai tra la: "<<a.tongTienLai();
+	
+
+
+	//CNganHang nh1;
+	//nh1.xuat();
+	//laythoigian tg1;
+	//tg1.xuat();
+
+		
+
+
 }
