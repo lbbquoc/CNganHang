683f7981c190fc0862c9652de6863d2cdfc36f42
minh thanh <minhthanhd013@gmail.com>
2020-04-22 18:29:35 +0700

    cpp

---
+++ b.cpp
@@ -0,0 +1,41 @@
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <string>
+#include "time.h"
+#include <stdlib.h> 
+//#include "Header.h"
+//#include "Header1.h"
+#include "Header2.h"
+#include "Header3.h"
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	
+	/*STK_KhongKyHan pp1;
+	pp1.nhap();
+	cout << pp1.tienLai() << endl;
+
+	
+	STK_CoKyHan pp2;
+	pp2.nhap();
+	cout << pp2.tienLai() << endl;*/
+	CNganHang a;
+	a.input();
+	
+	cout<<"Tong so tien lai ma ngan hang phai tra la: "<<a.tongTienLai();
+	
+
+
+	//CNganHang nh1;
+	//nh1.xuat();
+	//laythoigian tg1;
+	//tg1.xuat();
+
+		
+
+
+}
