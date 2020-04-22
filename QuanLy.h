f1da1ae74bf6cb957e28e55d7df0f28b0a019877
minh thanh <minhthanhd013@gmail.com>
2020-04-22 18:26:12 +0700

    sua gi do

--- QuanLy.h
+++ QuanLy.h
@@ -1,20 +1,57 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <time.h>
 #include <vector>
-#include "SoCoKyHan.h"
-#include "SoKhongKyHan.h"
+#include "Header3.h"
 
-class CNganHang
-{
-public:
-	CNganHang();
-	~CNganHang();
-	void Nhap();
-	void Xuat();
-	double TongTienLai_SoKKH(SoKKH);
-	double TongTienLai_SoCKH(SoCoKyHan);
+using namespace std;
+class STK_KhongKyHan {
 private:
-	vector<SoCoKyHan> _SoCoKyHan;
-	vector<SoKKH> _SoKhongKyHan;
+	long int soCMND;
+	string Hoten;
+	unsigned long long int soTien;
+	int Ngay;
+	int Thang;
+	int Nam;
+	int laiSuat;
+	int n;
+public:
+
+	void nhap()
+	{
+		cout << "So tiet kiem khong co ky han! " << endl;
+		cout << "Nhap so CMND: ";
+		cin >> this->soCMND;
+		cout << "Nhap ho ten: ";
+		cin >> this -> Hoten;
+		cout << "Nhap so tien (trieu dong): ";
+		cin >> this->soTien;
+		cout << "Nhap ngay/thang/nam ma ban bo tien vao ngan hang: ";
+		cin >> this->Ngay >> this->Thang >> this->Nam;
+		cout << "Nhap lai suat cua ngan hang (%): ";
+		cin >> this->laiSuat;
+	}
+
+	int tienLai()
+	{	
+		laythoigian tg1;
+		int year = tg1.getyear();
+		int month = tg1.getmonth();
+		int dif_year = year - this->Nam;
+		int dif_month = month - this->Thang;
+
+		if (dif_year == 0)
+		{
+			this->n = dif_month;
+		}
+		else
+		{
+			this->n = dif_year * 12 + dif_month;
+		}
+		
+		int tienlai = {};
+		double lai_ThapPhan = static_cast<double>(this->laiSuat) / 100;
+		tienlai = static_cast<int>(this->soTien * lai_ThapPhan) * (n);
+		return tienlai;
+	}
 };
-
-
